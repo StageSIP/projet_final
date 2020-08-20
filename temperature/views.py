dd48b229b2892f2dc8216d5c26a3519534796c79
ipkminu <ipkminu@gmail.com>
2020-08-20 05:22:52 +0100

    auth-1

--- temperature/views.py
+++ temperature/views.py
@@ -1,15 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 # Create your views here.
 
 def acceuil(request):
-    return render(request,'acceuil.html')
+    #si utilisateur connecté, retourner carte
+    if request.user.is_authenticated:
+        return render(request,'carte.html')
+    #sinon, retourner page de connexion
+    else:
+        return render(request,'acceuil.html')
 
 def carte(request):
-    return render(request,'carte.html')
+    #return render(request,'carte.html')
+    pass
 
-#def acceuil(request):
-#    return render(request,'acceuil.html')
+def connexion(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request,'carte.html')
 
-#def acceuil(request):
-#    return render(request,'acceuil.html')
+        else:
+            reponse= """
+            Vous n'êtes pas enregistré; Veuillez vous enregistrer d'abord!!!
+            """
+            return HttpResponse(reponse)
+    else:
+        return(acceuil(request))
+
+def inscription(request):
+    return render(request,'inscription.html')
+
+def deconnexion(request):
+    logout(request)
+    return(acceuil(request))
+
+
+
+def valider_inscription(request):
+    username=request.POST['username']
+    password=request.POST['password']
+    email=request.POST['email']
+    user = User.objects.create_user(username, email, password)
+    user.save()
+    return (acceuil(request))
